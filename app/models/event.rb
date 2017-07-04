f479d03bb0f90d5376b8ffe7711148f11b458ed9
DKateryna <derkach.kateryna@gmail.com>
2017-07-04 21:35:00 +0200

    One-to-Many: Models

---
+++ app/models/event.rb
@@ -0,0 +1,10 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :price, length: { minimum: 0 }
+  validates :includes_food, presence: false
+  validates :includes_drinks, presence: false
+  validates :active, presence: true
+  validates :description, presence: true, length: { maximum: 500 }
+end
