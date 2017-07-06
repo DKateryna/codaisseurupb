975903997220bf38314ba4b0a28987c5ab1b0b5f
DKateryna <derkach.kateryna@gmail.com>
2017-07-06 12:14:34 +0200

    Add CarrierWave and generate image uploader

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '2.4.1'
+
 git_source(:github) do |repo_name|
   repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
   "https://github.com/#{repo_name}.git"
@@ -54,6 +56,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5', '>= 3.5.2'
+
 end
 
 group :test do
@@ -68,3 +71,6 @@ gem 'jquery-rails', '~> 4.3.1'
 
 # Use Devise for authentication
 gem 'devise', '~> 4.2'
+
+gem 'rails_12factor', group: :production
+gem 'carrierwave', '0.11.2'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 User.destroy_all
 
 miriam = User.create!(email: "miriam@codaisseurupb.com", password: "abcd1234")
+tom = User.create(email: "tom@cat.com", password: "meowmeow")
 
 event = Event.create!(name: "Buite Westen",
   description: "Electronic and house music remains the primary focus of this multi-faceted festival.",
@@ -14,3 +15,15 @@ event = Event.create!(name: "Buite Westen",
   updated_at: DateTime.new(2017,7,4),
   active: true,
   user: miriam)
+
+  event2 = Event.create(name: "Neighborhood party",
+          description: "Summer Block Party: Meet With New Neighbors",
+          location: "Amsterdam",
+          price: 0,
+          capacity: 100,
+          includes_food: true,
+          includes_drinks: true,
+          starts_at: DateTime.new(2017, 7, 7),
+          ends_at: DateTime.new(2017, 7, 7),
+          active: true,
+          user: tom)
