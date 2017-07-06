16fb34554ce81626ac9a4b480b0aed529958c2c5
DKateryna <derkach.kateryna@gmail.com>
2017-07-06 15:36:41 +0200

    Create Photo: Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
+Photo.destroy_all
+#Theme.destroy_all
+Event.destroy_all
+#Profile.destroy_all
 User.destroy_all
 
 miriam = User.create!(email: "miriam@codaisseurupb.com", password: "abcd1234")
 tom = User.create(email: "tom@cat.com", password: "meowmeow")
+matt = User.create(email: "matt@codaisseurbnb.com", password: "abcd1234")
+
 
 event = Event.create!(name: "Buite Westen",
   description: "Electronic and house music remains the primary focus of this multi-faceted festival.",
@@ -16,14 +22,22 @@ event = Event.create!(name: "Buite Westen",
   active: true,
   user: miriam)
 
-  event2 = Event.create(name: "Neighborhood party",
-          description: "Summer Block Party: Meet With New Neighbors",
+  event2 = Event.create!(name: "Yoga lesson",
+          description: "If you want to do yoga, at any age, weight, or strength level, we welcome you.",
           location: "Amsterdam",
-          price: 0,
-          capacity: 100,
+          price: 10,
+          capacity: 15,
           includes_food: true,
           includes_drinks: true,
           starts_at: DateTime.new(2017, 7, 7),
           ends_at: DateTime.new(2017, 7, 7),
           active: true,
           user: tom)
+
+          # Photos
+      photo1 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djzs167i3/image/upload/v1499346114/images_lvhx7n.jpg", event: event)
+      photo2 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djzs167i3/image/upload/v1499346114/images_1_wcemqz.jpg", event: event )
+      photo3 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djzs167i3/image/upload/v1499346114/images_2_fjnt1i.jpg", event: event )
+
+      photo4 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djzs167i3/image/upload/v1499346508/images_4_cgl7li.jpg", event: event2)
+      photo5 = Photo.create!(remote_image_url: "http://res.cloudinary.com/djzs167i3/image/upload/v1499346508/images_3_yeepna.jpg", event: event2)
