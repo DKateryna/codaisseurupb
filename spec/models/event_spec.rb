edcf0c1d29329f88b26c5de47f30d7a15a321b4a
DKateryna <derkach.kateryna@gmail.com>
2017-07-10 21:34:19 +0200

    added model specs: associations

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  describe 'association with user' do
+    let(:user) { create :user }
+
+    it { is_expected.to belong_to :user }
+    it { is_expected.to have_and_belong_to_many :categories }
+  end
+
   describe 'validations' do
     it 'is invalid without name' do
       event = Event.new(name: '')
@@ -23,12 +30,26 @@ RSpec.describe Event, type: :model do
   end
 
   describe '.order_by_price' do
-    let!(:event1) { create :evetn, price: 100 }
-    let!(:event2) { create :evetn, price: 200 }
-    let!(:event3) { create :evetn, price: 50 }
+    let!(:event1) { create :event, price: 100 }
+    let!(:event2) { create :event, price: 200 }
+    let!(:event3) { create :event, price: 50 }
 
     it 'returns a sorted array of events by prices' do
       expect(Event.order_by_price).to eq([event3, event1, event2])
+    end
+  end
+
+  describe 'association with category' do
+    let(:event) { create :event }
+
+    let(:category1) { create :category, name: 'Bright', events: [event] }
+    let(:category2) { create :category, name: 'Clean lines', events: [event] }
+    let(:category3) { create :category, name: "A Man's Touch", events: [event] }
+
+    it 'has themes' do
+      expect(event.categories).to include(category1)
+      expect(event.categories).to include(category2)
+      expect(event.categories).to include(category3)
     end
   end
 end
